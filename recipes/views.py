2cc1390e93a446d09d51ebe0e33c8015a836b402
Luizedu10s <luiz-souza.ls@academico.ifpb.edu.gov>
2024-01-03 20:18:23 -0300

    Separando os templates da home e recipe

--- recipes/views.py
+++ recipes/views.py
@@ -6,4 +6,4 @@ def home(request):
     return render(request, 'recipes/pages/home.html')
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/recipe-view.html')
